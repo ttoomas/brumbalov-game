3faa7d4084a33eaa87ae89e245863e03baf55b5e
ttoomas <folprecht.to@gmail.com>
2022-12-08 21:44:52 +0100

    half of house zoom in animations

--- res/js/main.js
+++ res/js/main.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import gsap from "gsap";
+import gsap, { Power3 } from "gsap";
 import createScene from "./createScene";
 import Stats from 'three/examples/jsm/libs/stats.module';
 
@@ -30,9 +30,6 @@ let houseObject;
 let homeRaycaster = new THREE.Raycaster();
 let homeMouse = new THREE.Vector2();
 
-let selectedObject;
-let parent;
-
 
 // THREE INIT
 let controlsVar;
@@ -51,14 +48,8 @@ async function homeSceneInit(){
     // Functions
     await importHouseModel();
 
-    selectedObject = houseObject.getObjectByName("Book_Open_Vocabulary_1");
-    parent = selectedObject.parent;
-    parent.remove(selectedObject);
-
-    // selectedObject = houseObject.getObjectByName("Book_Open_Vocabulary_2");
-    // parent = selectedObject.parent;
-    // parent.remove(selectedObject);
-        
+    // Change transparent, delete book and text
+    updateHomeModel();        
 
     controlsFunction();
 
@@ -77,6 +68,60 @@ async function importHouseModel(){
     homeScene.add(houseObject);
 }
 
+// Change house model transparent, delete book
+let bookCoverMesh;
+let bookTextMesh;
+let bookParent;
+let doorTextMesh;
+let doorTextParent;
+let arsenalTextMesh;
+let arsenalTextParent;
+let healCauldronTextMesh;
+let healCauldronTextParent;
+
+function updateHomeModel(){
+    // TODO - mesh.parent.name - smaze cely mesh (asi)
+
+    // Book cover
+    bookCoverMesh = deleteMeshFromHouse(bookCoverMesh, "Book_Open_Vocabulary_1");
+
+    bookParent = bookCoverMesh.parent;
+    bookParent.remove(bookCoverMesh);
+
+    // Book text
+    bookTextMesh = deleteMeshFromHouse(bookTextMesh, "Book_Open_Vocabulary_2");
+
+    bookParent.remove(bookTextMesh);
+
+    // Door Text
+    doorTextMesh = deleteMeshFromHouse(doorTextMesh, "Brick_Door_7");
+
+    doorTextParent = doorTextMesh.parent;
+    doorTextParent.remove(doorTextMesh);
+
+    // Arsenal Text
+    arsenalTextMesh = deleteMeshFromHouse(arsenalTextMesh, "Arsenal_Change_10");
+
+    arsenalTextParent = arsenalTextMesh.parent;
+    arsenalTextParent.remove(arsenalTextMesh);
+
+    // Heal Cauldron Text
+    healCauldronTextMesh = deleteMeshFromHouse(healCauldronTextMesh, "Heal_Cauldron_2");
+
+    healCauldronTextParent = healCauldronTextMesh.parent;
+    healCauldronTextParent.remove(healCauldronTextMesh);
+}
+
+function deleteMeshFromHouse(variable, meshObjectName){
+    variable = houseObject.getObjectByName(meshObjectName);
+    variable.material.transparent = true;
+    variable.material.depthWrite = false;
+    variable.material.opacity = 0;
+
+    return variable;
+}
+
+
 // Mooving house on cursor move
 function bodyMouseMove(){
     document.body.addEventListener('mousemove', (e) => {
@@ -90,6 +135,13 @@ function bodyMouseMove(){
 
 
 // HOME ONCLICK
+let activeVocabulary = false;
+let activeDoor = false;
+let activeArsenal = false;
+let activeHealCauldron = false;
+
+checkEscToResetCamera();
+
 function handleHomeClick(e){
     e.preventDefault();
 
@@ -104,46 +156,117 @@ function handleHomeClick(e){
         let object = intersects[0].object;
 
         console.log(object);
+
+        // ACTIVE
+        // Active Vocabulary
+        if(activeVocabulary) activeVocabularyHandler(object);
+        if(activeDoor) activeDoorHandler(object);
+
     
         // Vocabulary table
         if(object.name.includes('Vocabulary_Table_')){
             console.log('Table');
 
+            activeVocabulary = true;
+
             // Change camera position
             gsap.to(homeCamera.position, {
                 duration: 1,
-                x: 3.1770095122945516,
-                y: 0.6126561661774869,
-                z: 2.845686434734792
+                x: 2.927868438389019,
+                y: 0.7513829103299781,
+                z: 2.872406515072331
             })
 
             gsap.to(controlsVar.target, {
                 duration: 1,
-                x: 0.5206104737929833,
-                y: -0.44962723446350095,
-                z: 3.6240351733161136
+                x: 0.5699371461463288,
+                y: -0.6995119320075021,
+                z: 3.390770266589537
             })
 
             setTimeout(() => {
-                parent.add(selectedObject);
-                selectedObject.material.transparent = true;
-                selectedObject.material.opacity = 0;
-                gsap.to(selectedObject.material, {
-                    duration: 0.5,
+                // Add book cover
+                bookParent.add(bookCoverMesh);
+                gsap.to(bookCoverMesh.material, {
+                    duration: 0.8,
+                    opacity: 1
+                })
+
+                // Add book text
+                bookParent.add(bookTextMesh);
+                gsap.to(bookTextMesh.material, {
+                    duration: 0.8,
                     opacity: 1
                 })
             }, 1100);
-
         }
 
         // Doors
         if(object.name.includes('Brick_Door_')){
             console.log('Door');
+
+            activeDoor = true;
+
+            // Change Camera position
+            gsap.to(homeCamera.position, {
+                duration: 1,
+                x: 1.8561061250509379,
+                y: 0.8746107905845485,
+                z: 0.32270146302434405
+            })
+
+            gsap.to(controlsVar.target, {
+                duration: 1,
+                x: -0.23839009429976987,
+                y: 0.79998877865599,
+                z: 0.3056575530841469
+            })
+
+            // TODO - oncomplete - add active door to true
+            // - when we zoom in and fast press esc, mesh (book...) will stay on screen
+            // to it everywhere to replace settimeout after the animation is done
+
+            setTimeout(() => {
+                // Add door text
+                doorTextParent.add(doorTextMesh);
+
+                gsap.to(doorTextMesh.material, {
+                    duration: 0.5,
+                    opacity: 1
+                })
+            }, 1000);
         }
 
         // Arsenal
         if(object.name.includes('Arsenal_Change_')){
             console.log('Arsenal');
+
+            activeArsenal = true;
+
+            // Change camera position
+            gsap.to(homeCamera.position, {
+                duration: 1,
+                x: 2.727204473525794,
+                y: 0.4604903396870391,
+                z: -0.4367519428174377
+            })
+            
+            gsap.to(controlsVar.target, {
+                duration: 1,
+                x: 0.09592168431359922,
+                y: -0.17092384230219762,
+                z: -0.556754032928711
+            })
+
+            setTimeout(() => {
+                // Add arsenal text
+                arsenalTextParent.add(arsenalTextMesh);
+
+                gsap.to(arsenalTextMesh.material, {
+                    duration: 0.4,
+                    opacity: 1
+                })
+            }, 1000);
         }
 
         // Cauldron
@@ -152,6 +275,94 @@ function handleHomeClick(e){
         }
     }
 }
+
+
+// Active Vocabulary handler
+function activeVocabularyHandler(object){
+    if(object.name.includes('Vocabulary_Table_')) return;
+    else if(object.name.includes('Book_Open_Vocabulary_')){
+        console.log('clicked on vocabulary book');
+    }
+    else{
+        resetHomeCamera();
+        bookDelete();
+    }
+}
+
+// Active door handler
+function activeDoorHandler(object){
+    console.log(object);
+}
+
+
+// Helpers
+function checkEscToResetCamera(){
+    window.addEventListener('keydown', (e) => {
+        if(e.code === "Escape" && activeVocabulary){
+            resetHomeCamera();
+            bookDelete();
+        }
+        else if(e.code === "Escape" && activeDoor){
+            resetHomeCamera();
+            homeMeshTextDelete(doorTextMesh, doorTextParent);
+        }
+        else if(e.code === "Escape" && activeArsenal){
+            resetHomeCamera();
+            homeMeshTextDelete(arsenalTextMesh, arsenalTextParent)
+        }
+    })
+}
+
+function bookDelete(){
+    gsap.to(bookCoverMesh.material, {
+        duration: 0.5,
+        opacity: 0
+    })
+    
+    // Add book text
+    gsap.to(bookTextMesh.material, {
+        duration: 0.5,
+        opacity: 0,
+        onComplete: function(){
+            bookParent.remove(bookCoverMesh);
+            bookParent.remove(bookTextMesh);
+        }
+    })
+}
+
+function homeMeshTextDelete(deleteMesh, deleteParent,){
+    gsap.to(deleteMesh.material, {
+        duration: 0.3,
+        opacity: 0,
+        onComplete: function(){
+            deleteParent.remove(deleteMesh);
+        }
+    })
+}
+
+
+// Function to reset home camera settings
+function resetHomeCamera(){
+    activeVocabulary = false;
+    activeDoor = false;
+
+    gsap.to(homeCamera.position, {
+        duration: 1,
+        ease: "slow(0.7, 0.7, false)",
+        x: 6.5,
+        y: 2,
+        z: 0.5
+    })
+
+    gsap.to(controlsVar.target, {
+        duration: 1,
+        ease: "slow(0.7, 0.7, false)",
+        x: 0,
+        y: 0,
+        z: 0
+    })
+}
+
 
 
 // ANIMATE
@@ -173,11 +384,11 @@ let stats;
 function controlsFunction(){
     controlsVar.enabled = false;
     
-    controlsVar.addEventListener('change', () => {
-        console.log(homeCamera.position);
-        console.log('target (lookAt)');
-        console.log(controlsVar.target);
-    })
+    // controlsVar.addEventListener('change', () => {
+    //     console.log(homeCamera.position);
+    //     console.log('target (lookAt)');
+    //     console.log(controlsVar.target);
+    // })
     
     stats = new Stats();
     stats.showPanel(0);
